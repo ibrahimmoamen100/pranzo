d1c1bd1b109b7cc2a44716b01f79a20c0de44792
ibrahimmoamen100 <ibrahim.moamen100@gmail.com>
2025-06-20 03:08:16 +0300

    new update

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -235,7 +235,7 @@ export default function Products() {
                 </p>
               </div>
             ) : (
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+              <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                 {currentProducts.map((product) => (
                   <ProductCard
                     key={product.id}
